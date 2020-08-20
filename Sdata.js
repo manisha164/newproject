cb0a3e20e97eed6d5cba5d41771c33d5fbf897cc
manisha164 <69892465+manisha164@users.noreply.github.com>
2020-08-20 15:51:54 +0530

    Add files via upload

---
+++ Sdata.js
@@ -0,0 +1,55 @@
+
+import Web from '../src/images/web.png'
+
+import Marke from '../src/images/marketing.png'
+import Web1 from '../src/images/webimg.png'
+import Ecomrce from '../src/images/ecommerce.png'
+import Service from '../src/images/service.png'
+import Content from '../src/images/content.png'
+import Social from '../src/images/social.png'
+import Payper from '../src/images/payPer.png'
+
+
+const Sdata =[
+    {
+        imgsrc:Web,
+        title:'Web Design'
+    },
+
+
+    {
+        imgsrc:Marke,
+        title:'Online Marketing'
+    },
+
+    {
+        imgsrc:Web1,
+        title:'Web And Email Hosting '
+    },
+
+    {
+        imgsrc:Ecomrce,
+        title:'Ecommerce'
+    },
+
+    {
+        imgsrc:Service,
+        title:'SEO'
+    },
+
+    {
+        imgsrc:Content,
+        title:'Content Writing'
+    },
+
+    {
+        imgsrc:Social,
+        title:'Social Media Marketing'
+    },
+
+    {
+        imgsrc:Payper,
+        title:'Pay Per Click'
+    },
+]
+export default Sdata
